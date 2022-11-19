d9f95540f74cc88075892f7121b2c886da733088
alanfsh <zero_ikniz@hotmail.com>
2022-11-19 14:58:52 -0600

    home finished

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def login():
     )
 def show_all_users():
     """
-    SHow all users
+    Show all users
 
     This path operation shows all  users in the app
 
@@ -188,7 +188,24 @@ def update_a_user():
     tags=["Tweets"]
     )
 def home():
-    return {"Twitter-API": "Working!"}
+    """
+    Show all users
+
+    This path operation shows all  tweets in the app
+
+    Parameters:
+        -
+
+    Returns a json with all tweets in the app, with the following keys:
+        - tweet_id: UUID
+        - content: str
+        - created_at: datetime
+        - updated_at: Optional[datetime]
+        - by: User
+    """
+    with open("tweets.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 ### Post a tweet
 @app.post(
